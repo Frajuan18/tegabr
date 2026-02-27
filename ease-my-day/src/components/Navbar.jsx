48b73bc5a4601f38a3d1530b01eb0c2f192031fe
Frajuan18 <fraolabmas@gmail.com>
2026-02-27 11:32:45 +0300

    m

--- ease-my-day/src/components/Navbar.jsx
+++ ease-my-day/src/components/Navbar.jsx
@@ -266,7 +266,7 @@ export default function Navbar() {
                   Log in
                 </Link>
                 <Link
-                  to="/register"
+                  to="/signup"
                   className="px-5 py-2.5 text-sm font-semibold text-white bg-[#1C1C1E] rounded-2xl shadow-[0_8px_0_#000000,0_4px_12px_rgba(0,0,0,0.2)] hover:shadow-[0_4px_0_#000000,0_8px_16px_rgba(0,0,0,0.3)] hover:translate-y-1 transition-all duration-200"
                 >
                   Sign up
